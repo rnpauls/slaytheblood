b5d9c3bb1a4ad1ee2d55e8ab3c3826207eabf275
rnpauls <rnpauls@gmail.com>
2025-01-07 22:02:10 -0500

    s02e08

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,8 +4,10 @@ extends Node2D
 const WHITE_SPRITE_MATERIAL := preload("res://art/white_sprite_material.tres")
 
 @export var stats: CharacterStats : set = set_character_stats
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var stats_ui: StatsUI = $StatsUI as StatsUI
+@onready var status_handler: StatusHandler = $StatusHandler
 
 #func _ready() -> void:
 	#await get_tree().create_timer(3).timeout
