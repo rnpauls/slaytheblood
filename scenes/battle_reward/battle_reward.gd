3095a1ff79187ca7ca186251db2f19b32d48e0d0
rnpauls <rnpauls@gmail.com>
2025-01-18 23:54:15 -0500

    S02E12

--- scenes/battle_reward/battle_reward.gd
+++ scenes/battle_reward/battle_reward.gd
@@ -10,7 +10,7 @@ const CARD_TEXT := "Add New Card"
 
 @export var run_stats: RunStats
 @export var character_stats: CharacterStats
-#@export var relic_handler: RelicHandler
+@export var relic_handler: RelicHandler
 
 
 @onready var rewards: VBoxContainer = %Rewards
@@ -39,6 +39,16 @@ func add_card_reward() -> void:
 	card_reward.reward_text = CARD_TEXT
 	card_reward.pressed.connect(_show_card_rewards)
 	rewards.add_child.call_deferred(card_reward)
+
+func add_relic_reward(relic: Relic) -> void:
+	if not relic:
+		return
+
+	var relic_reward := REWARD_BUTTON.instantiate() as RewardButton
+	relic_reward.reward_icon = relic.icon
+	relic_reward.reward_text = relic.relic_name
+	relic_reward.pressed.connect(_on_relic_reward_taken.bind(relic))
+	rewards.add_child.call_deferred(relic_reward)
 
 func _show_card_rewards() -> void:
 	if not run_stats or not character_stats:
@@ -96,6 +106,12 @@ func _on_card_reward_taken(card: Card) -> void:
 		return
 		
 	character_stats.deck.add_card(card)
+
+func _on_relic_reward_taken(relic: Relic) -> void:
+	if not relic or not relic_handler:
+		return
+		
+	relic_handler.add_relic(relic)
 
 func _on_back_button_pressed() -> void:
 	Events.battle_reward_exited.emit()
--- scenes/treasure/treasure.gd
+++ scenes/treasure/treasure.gd
@@ -1,5 +1,26 @@
+class_name Treasure
 extends Control
 
+@export var treasure_relic_pool: Array[Relic]
+@export var relic_handler: RelicHandler
+@export var char_stats: CharacterStats
 
-func _on_button_pressed():
-	Events.treasure_room_exited.emit()
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+var found_relic: Relic
+
+func generate_relic() -> void:
+	var available_relics := treasure_relic_pool.filter(
+		func(relic: Relic):
+			var can_appear := relic.can_appear_as_reward(char_stats)
+			var already_had_it := relic_handler.has_relic(relic.id)
+			return can_appear and not already_had_it
+	)
+	found_relic = available_relics.pick_random()
+
+#called from animation at end of 'open'
+func _on_treasure_opened() -> void:
+	Events.treasure_room_exited.emit(found_relic)
+
+func _on_treasure_chest_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_mouse"):
+		animation_player.play("open")
